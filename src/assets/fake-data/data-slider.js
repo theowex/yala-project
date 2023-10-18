eb01aa1bb4ce4c995af39bb53b1f6815cd3f7142
Owen Atamari <owenatamari@gmail.com>
2023-10-17 22:35:49 -0500

    primer commit

---
+++ src/assets/fake-data/data-slider.js
@@ -0,0 +1,38 @@
+import img1 from '../images/slider/slide_1.png';
+import img2 from '../images/slider/slide_1.png';
+import imgbg1 from '../images/slider/bg_slide_1.png'
+import imgbg2 from '../images/slider/bg_slide_1.png'
+
+
+const heroSliderData = [
+    {
+        title_1: "Compra y subasta tus",
+        title_2: "coleccionables",
+        title_3: "favoritos",
+        description: "El marketplace para encontrar tus Funko Pop!",
+        img: img1,
+        imgbg: imgbg1,
+        class:'left'
+    },
+    {
+        title_1: "Compra y subasta tus",
+        title_2: "coleccionables",
+        title_3: "favoritos",
+        description: "El marketplace para encontrar tus Funko Pop!",
+        img: img2,
+        imgbg: imgbg2,
+        class:'center'
+    },
+    {
+        title_1: "Compra y subasta tus",
+        title_2: "coleccionables",
+        title_3: "favoritos",
+        description: "El marketplace para encontrar tus Funko Pop!",
+        img: img2,
+        imgbg: imgbg2,
+        class:'right'
+    },
+    
+]
+
+export default heroSliderData;
